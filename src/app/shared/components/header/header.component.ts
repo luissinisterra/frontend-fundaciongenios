c5efb61e37ad5710d033cf36544d9b00a1f3d5bb
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-01-23 17:11:17 -0500

    Mejora en el header para mobiles y en el header para pc

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
