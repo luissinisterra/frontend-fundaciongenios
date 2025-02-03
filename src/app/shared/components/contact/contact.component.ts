ba1d604c2782411f71caa1985b4c0be82c40479b
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-02-02 22:09:11 -0500

    Formulario reactivo para el contacto de la web

--- src/app/shared/components/contact/contact.component.ts
+++ src/app/shared/components/contact/contact.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+  successMessage?: string;
+  showAlert: boolean = false;
+
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.successMessage = '¡Gracias por tu mensaje!';
+      this.showAlert = true;
+
+      setTimeout(() => {
+        this.showAlert = false;
+      }, 2000);
+
+      this.contactForm.reset();
+    }
+  }
 }
